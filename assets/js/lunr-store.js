94d66ec89893ad8a0e83604723e1c40b6be8a806
Michael Andrec <mandrecphd@gmail.com>
2024-10-27 12:32:34 -0400

    Initial commit

---
+++ assets/js/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{  
+"title": "Rae Armantrout Interview on Writing Practices","date": "2014-03-27","description": "Interview with Rae Armantrout in San Diego, CA. Featured in the digital scholarship exhibit CTRL+Shift (ctrl-shift.html)","subject": "poetry;writing practice;ipads;computers and writing;notebooks","location": "San Diego, CA", 
+"id": "armantrout.html"
+
+},{  
+"title": "Louise Glück Interview – descriptions only","date": "2014-05-15","description": "Interview with Louise Glück in Cambridge, MA. Featured in the digital scholarship exhibit CTRL+Shift (ctrl-shift.html)","subject": "poetry;writing practice;ipads;computers and writing","location": "Cambridge, MA", 
+"id": "gluck-tp.html"
+
+},{  
+"title": "Louise Glück Interview – descriptions and transcript","date": "2014-05-15","description": "Interview with Louise Glück in Cambridge, MA. Featured in the digital scholarship exhibit CTRL+Shift (ctrl-shift.html)","subject": "poetry;writing practice;ipads;computers and writing","location": "Cambridge, MA", 
+"id": "gluck.html"
+
+},{  
+"title": "K. Silem Mohammad Interview on Writing Practices","date": "2014-06-14","description": "Interview with K. Silem Mohammad in Ashland, OR. Featured in the digital scholarship exhibit CTRL+Shift (ctrl-shift.html)","subject": "poetry;writing practice;flarf;computers and writing","location": "Ashland, OR", 
+"id": "mohammad.html"
+
+},{  
+"title": "Robert Wrigley Interview on Writing Practices","date": "2013","description": "Interview with Robert Wrigley in Moscow, ID. Featured in the digital scholarship exhibit CTRL+Shift (ctrl-shift.html)","subject": "poetry;writing practice;notebooks;computers and writing","location": "Moscow, ID", 
+"id": "wrigley.html"
+
+}];
